117ceed4c97f0eec112097aae70db47c4125c827
Ishan Karpe <ishan.k.abhijeet@gmail.com>
2025-03-03 19:24:52 -0800

    Update main.py with conditons

--- main.py
+++ main.py
@@ -18,3 +18,30 @@ from functools import lru_cache
 # Now, create a function called fizzbuzz() that takes a single parameter, amount, and follows the rules above"""
 
 
+amount = int(input("Enter a number: "))
+@lru_cache()
+def fizzbuzz(amount):
+    for i in range(1, amount + 1):
+
+        # the twist
+        """Numbers that contain the digit "3" but aren't divisible by 3 or 7 will output Almost Fizz"""
+
+        if ("3" in str(i)) and (i % 3 != 0 and i % 7 != 0):
+            print("Almost Fizz")
+            continue
+
+        elif (i % 3 == 0) and (i % 7 == 0):  # Both
+            print("FizzBuzz")
+            continue
+
+        elif i % 3 == 0:  # One
+            print("Fizz")
+            continue
+
+        elif i % 7 == 0:  # One
+            print("Buzz")
+            continue
+
+        else:
+            print(i)  # if none of the above conditions are met, then print the increment.
+            continue
